669433ac48e569d36f842c56249680a3420f0c9a
Shubham Mamgain <shubhammamgain614@gmail.com>
2020-06-15 14:00:01 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component, Fragment } from "react";
+
+import "./App.css";
+import EventDashboard from "./features/event/EventDashboard/EventDashboard";
+import Navbar from "./features/nav/Navbar/Navbar";
+import { Container } from "semantic-ui-react";
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Navbar />
+        <Container className="main">
+          <EventDashboard />
+        </Container>
+      </Fragment>
+    );
+  }
+}
+
+export default App;
